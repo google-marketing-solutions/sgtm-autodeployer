a1baa34dcee54f442f1f88e75272f6d9442b40c7
Miguel Fernandes <miguelfc@google.com>
2023-05-10 14:06:06 +0200

    Updated deployment script.
    Change-Id: Ib0b18db1f321be2d60b5ea9c3f58053e076f5aff

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script deploys sGTM using custom settings.
+# This script deploys the tagging server using custom settings.
 #
 # Copyright 2023 Google LLC
 #
@@ -21,33 +21,26 @@ set -e
 CWD="$(dirname "$(realpath "$0")")"
 source "${CWD}/config.conf"
 
+IMAGE="gcr.io/cloud-tagging-10302018/gtm-cloud-image:stable"
+MIN_INSTANCES=2
+
 function gcloud_ext() {
   if [ -n "${OPT_DEBUG}" ]
   then
     echo "Skipped command as we are running in debug mode: "
-    echo gcloud $@
+    echo gcloud "$@"
   else
-    gcloud $@
+    gcloud "$@"
   fi
 }
 
 function gcloud_int() {
   if [ -n "${OPT_DEBUG}" ]
   then
-    echo gcloud $@
+    echo gcloud "$@"
   else
-    gcloud $@
+    gcloud "$@"
   fi
-}
-
-function print_usage() {
-  echo "Server Side GTM Autodeployer."
-  echo "Usage: deploy.sh [-t] [-u] [-p project-id]"
-  echo ""
-  echo "Options:"
-  echo "-t:                    Execute in test mode (no changes will be made)."
-  echo "-u:                    Delete sGTM instead of deploying it."
-  echo "-p project-id:         Id of the GCP project where sGTM will be deployed."
 }
 
 function cli_errors() {
@@ -88,9 +81,14 @@ function create_address() {
   gcloud_ext compute addresses create --global ${SERVICE_IP}
 }
 
+function describe_address() {
+  echo -e "This is the IP associated with your load balancer:"
+  gcloud_ext compute addresses describe --global ${SERVICE_IP} --format='value(address)'
+}
+
 function delete_address() {
   echo -e "\n--> Deleting the IP address..."
-  gcloud_ext compute addresses delete --global ${SERVICE_IP} ${FORCE}
+  gcloud_ext compute addresses delete --global ${SERVICE_IP} ${FORCE} || :
 }
 
 function create_backend_service() {
@@ -100,7 +98,33 @@ function create_backend_service() {
 
 function delete_backend_service() {
   echo -e "\n--> Deleting the backend service..."
-  gcloud_ext compute backend-services delete --global ${BACKEND_NAME} ${FORCE}
+  gcloud_ext compute backend-services delete --global ${BACKEND_NAME} ${FORCE} || :
+}
+
+function create_script_serving_backend_service() {
+  echo -e "\n--> Creating the backend service for script serving..."
+  gcloud_ext compute backend-services create ${SCRIPT_SERVING_BACKEND_NAME} --global \
+  --custom-request-header "X-Gclb-Country: {client_region}" \
+  --custom-request-header "X-Gclb-Region: {client_region_subdivision}"
+
+  for REGION in ${REGIONS_LIST[@]}
+  do
+    gcloud_ext compute backend-services add-backend ${SCRIPT_SERVING_BACKEND_NAME} \
+    --network-endpoint-group=server-side-tagging-neg --global \
+    --network-endpoint-group-region="${REGION}"
+  done
+
+  gcloud_ext compute url-maps add-path-matcher ${URLMAP_NAME} \
+  --path-matcher-name="${SCRIPT_SERVING_BACKEND_NAME}" \
+  --path-rules="/gtm.js=${SCRIPT_SERVING_BACKEND_NAME},/gtags/*=${SCRIPT_SERVING_BACKEND_NAME}" \
+  --default-service="${BACKEND_NAME}"
+}
+
+function delete_script_serving_backend_service() {
+  echo -e "\n--> Deleting the backend service for script serving..."
+  gcloud_ext compute url-maps remove-path-matcher ${URLMAP_NAME} \
+  --path-matcher-name="${SCRIPT_SERVING_BACKEND_NAME}" || :
+  gcloud_ext compute backend-services delete ${SCRIPT_SERVING_BACKEND_NAME} --global ${FORCE} || :
 }
 
 function create_url_map() {
@@ -110,7 +134,7 @@ function create_url_map() {
 
 function delete_url_map() {
   echo -e "\n--> Deleting the URL map..."
-  gcloud_ext compute url-maps delete ${URLMAP_NAME} ${FORCE}
+  gcloud_ext compute url-maps delete ${URLMAP_NAME} ${FORCE} || :
 }
 
 function create_tls_certificate() {
@@ -121,7 +145,7 @@ function create_tls_certificate() {
 
 function delete_tls_certificate() {
   echo -e "\n--> Deleting the managed TLS Certificate..."
-  gcloud_ext compute ssl-certificates delete ${CERT_NAME} ${FORCE}
+  gcloud_ext compute ssl-certificates delete ${CERT_NAME} ${FORCE} || :
 }
 
 
@@ -133,7 +157,7 @@ function create_target_https_proxy() {
 }
 function delete_target_https_proxy() {
   echo -e "\n--> Delete the target HTTPS Proxy..."
-  gcloud_ext compute target-https-proxies delete ${HTTPS_PROXY_NAME} ${FORCE}
+  gcloud_ext compute target-https-proxies delete ${HTTPS_PROXY_NAME} ${FORCE} || :
 }
 
 function create_forwarding_rule() {
@@ -146,12 +170,12 @@ function create_forwarding_rule() {
 
 function delete_forwarding_rule() {
   echo -e "\n--> Delete the forwarding rule..."
-  gcloud_ext compute forwarding-rules delete --global ${FORWARDING_RULE_NAME} ${FORCE}
+  gcloud_ext compute forwarding-rules delete --global ${FORWARDING_RULE_NAME} ${FORCE} || :
 }
 
 function deploy_cloud_run() {
-  # Deploy the sGTM preview application only once
-  echo -e "\n--> Deploy the sGTM preview application in ${REGIONS_LIST[0]}..."
+  # Deploy the tagging server preview application only once
+  echo -e "\n--> Deploy the tagging server preview application in ${REGIONS_LIST[0]}..."
   gcloud_ext run deploy "${SERVICE_NAME}-preview" \
   --region ${REGIONS_LIST[0]} \
   --image ${IMAGE} \
@@ -163,36 +187,42 @@ function deploy_cloud_run() {
   --update-env-vars \
   RUN_AS_PREVIEW_SERVER=true,CONTAINER_CONFIG="${CONTAINER_CONFIG}"
 
-  PREVIEW_SERVER_URL=$(
-      gcloud_int run services describe ${SERVICE_NAME}-preview --region "${REGIONS_LIST[0]}" \
-      --format="value(status.url)")
-  for REGION in ${REGIONS_LIST[@]}
-  do
-    # Deploy the sGTM application
-    echo -e "\n--> Deploy the sGTM application in ${REGION}..."
-    gcloud_ext run deploy "${SERVICE_NAME}" \
-    --region ${REGION} \
-    --image ${IMAGE} \
-    --platform managed \
-    --ingress all \
-    --min-instances ${MIN_INSTANCES} \
-    --max-instances ${MAX_INSTANCES} \
-    --timeout 60 \
-    --allow-unauthenticated \
-    --no-cpu-throttling \
-    --update-env-vars PREVIEW_SERVER_URL="${PREVIEW_SERVER_URL}",CONTAINER_CONFIG="${CONTAINER_CONFIG}"
-  done
+  if [[ "${MODE}" != "deploy_testing" ]]
+  then
+    PREVIEW_SERVER_URL=$(
+        gcloud_int run services describe ${SERVICE_NAME}-preview --region "${REGIONS_LIST[0]}" \
+        --format="value(status.url)")
+    for REGION in ${REGIONS_LIST[@]}
+    do
+      # Deploy the tagging server
+      echo -e "\n--> Deploy the tagging server in ${REGION}..."
+      gcloud_ext run deploy "${SERVICE_NAME}" \
+      --region ${REGION} \
+      --image ${IMAGE} \
+      --platform managed \
+      --ingress all \
+      --min-instances ${MIN_INSTANCES} \
+      --max-instances ${MAX_INSTANCES} \
+      --timeout 60 \
+      --allow-unauthenticated \
+      --no-cpu-throttling \
+      --update-env-vars PREVIEW_SERVER_URL="${PREVIEW_SERVER_URL}",CONTAINER_CONFIG="${CONTAINER_CONFIG}"
+    done
+  fi
 }
 
 function delete_cloud_run() {
-  echo -e "\n--> Deleting the sGTM preview application in ${REGIONS_LIST[0]}..."
-  gcloud_ext run services delete "${SERVICE_NAME}-preview" ${FORCE} --region ${REGIONS_LIST[0]}
+  echo -e "\n--> Deleting the tagging server preview in ${REGIONS_LIST[0]}..."
+  gcloud_ext run services delete "${SERVICE_NAME}-preview" ${FORCE} --region ${REGIONS_LIST[0]} || :
 
-  for REGION in ${REGIONS_LIST[@]}
-  do
-    echo -e "\n--> Deleting the sGTM application in ${REGION}..."
-    gcloud_ext run services delete "${SERVICE_NAME}" ${FORCE} --region ${REGION}
-  done
+  if [[ "${MODE}" != "remove_testing" ]]
+  then
+    for REGION in ${REGIONS_LIST[@]}
+    do
+      echo -e "\n--> Deleting the tagging server in ${REGION}..."
+      gcloud_ext run services delete "${SERVICE_NAME}" ${FORCE} --region ${REGION} || :
+    done
+  fi
 }
 
 function create_network_endpoint_groups() {
@@ -215,17 +245,108 @@ function delete_network_endpoint_groups() {
     echo -e "\n--> Deleting the network endpoint group for ${REGION}..."
     gcloud_ext compute backend-services remove-backend ${FORCE} --global "${BACKEND_NAME}" \
     --network-endpoint-group-region=${REGION} \
-    --network-endpoint-group="${SERVICE_NAME}-neg"
-    gcloud_ext compute network-endpoint-groups delete ${SERVICE_NAME}-neg ${FORCE} --region=${REGION}
+    --network-endpoint-group="${SERVICE_NAME}-neg" || :
+    gcloud_ext compute network-endpoint-groups delete ${SERVICE_NAME}-neg ${FORCE} --region=${REGION} || :
   done
 }
 
+function create_log_filters() {
+  if [ -n "${LOGGING}" ]
+  then
+    echo -e "\n--> Create the log filters..."
+    if [[ "${LOGGING}" == "DISABLE" ]]
+    then
+      gcloud_ext logging sinks update _Default \
+      --add-exclusion='name=run-server,filter=LOG_ID("run.googleapis.com/requests")'
+      gcloud_ext logging sinks update _Default \
+      --add-exclusion='name=balancer-server,filter=LOG_ID("requests")'
+    elif [[ "${LOGGING}" == "ERROR_ONLY" ]]
+    then
+      gcloud_ext logging sinks update _Default \
+      --add-exclusion='name=run-server,filter=LOG_ID("run.googleapis.com/requests") AND severity<=ERROR'
+      gcloud_ext logging sinks update _Default \
+      --add-exclusion='name=balancer-server,filter=LOG_ID("requests") AND severity<=ERROR'
+    fi
+  fi
+}
+
+function delete_log_filters() {
+  echo -e "\n--> Deleting the log filters..."
+  gcloud_ext logging sinks update _Default --remove-exclusions='run-filter' || :
+  gcloud_ext logging sinks update _Default --remove-exclusions='balancer-filter' || :
+}
+
+function validate_values() {
+  IFS=',' read -ra REGIONS_LIST <<< ${REGIONS}
+
+  if [ -n "${PROJECT_ID}" ]
+  then
+    gcloud_ext config set project ${PROJECT_ID}
+    export GOOGLE_CLOUD_PROJECT=${PROJECT_ID}
+  elif [ -n "${GOOGLE_CLOUD_PROJECT}" ]
+  then
+    gcloud_ext config set project ${GOOGLE_CLOUD_PROJECT}
+  else
+    GOOGLE_CLOUD_PROJECT=$(gcloud config get project)
+    if [ -z "${GOOGLE_CLOUD_PROJECT}" ]
+    then
+      echo "Aborting execution: prerequisites have not been met."
+      echo "- Google Cloud Project not defined."
+      echo "  Please run 'gcloud config set project <PROJECT_ID>' and try again."
+      exit 1
+    fi
+  fi
+
+  if [ -z "${CONTAINER_CONFIG}" ]
+  then
+    echo "Aborting execution: prerequisites have not been met."
+    echo "- CONTAINER_CONFIG is required."
+    echo "  In Tag Manager, navigate to your server container workspace and click on"
+    echo "  the container ID at the top-right of the page. Click on "
+    echo "  'Manually provision tagging server' to find the Container Config value."
+    exit 1
+  fi
+
+  if [[ "${MODE}" == "deploy" && -z "${DOMAIN_NAME}" ]]
+  then
+    echo "Aborting execution: prerequisites have not been met."
+    echo "- DOMAIN_NAME is required for production deployments. Please fix and try again."
+    exit 1
+  fi
+
+  if [[ "${MODE}" == "deploy_testing" && ${#REGIONS_LIST[*]} -gt 1 ]]
+  then
+    echo "Aborting execution: prerequisites have not been met."
+    echo "- Multiple deployment regions is not supported for testing environments. Please fix and try again."
+    exit 1
+  fi
+
+}
+
+function print_usage() {
+  echo "Server Side GTM Autodeployer."
+  echo "Usage: deploy.sh [-m deploy|show_address|deploy_testing|delete] [-t] [-p project-id]"
+  echo ""
+  echo "Options:"
+  echo "-m                      Defines the operation to perform. Available options are:"
+  echo "   deploy                -> Default option, deploys the tagging server in production mode."
+  echo "   show_address          -> Displays the IP address that needs to be added to DNS record."
+  echo "   remove                -> Deletes the components (if any) associated with this deployment."
+  echo "   deploy_testing        -> Deploys the tagging server in testing mode (without load balancer)."
+  echo "   remove_testing        -> Deletes the testing mode components (if any) associated with this deployment."
+  echo "   script_serving        -> Deploys a specific backend service for first party script serving."
+  echo "   remove_script_serving -> Deletes the backend service used for first party script serving."
+  echo "-d                      Dry run mode (no changes will be made)."
+  echo "-p project-id           Id of the GCP project where the tagging server will be deployed."
+}
+
 function main() {
-    while getopts 'tp:ufh' arg; do
+    MODE="deploy"
+    while getopts 'dm:p:fh' arg; do
         case "${arg}" in
-            t) OPT_DEBUG="true";;
+            d) OPT_DEBUG="true";;
+            m) MODE="${OPTARG}";;
             p) PROJECT_ID="${OPTARG}";;
-            u) DELETE="true";;
             f) FORCE="--quiet";;
             h) print_usage; exit 0;;
             *) cli_errors; exit 1;;
@@ -233,64 +354,50 @@ function main() {
     done
     shift "$((OPTIND-1))"
 
-    if [ -n "${PROJECT_ID}" ]
-    then
-      gcloud_ext config set project ${PROJECT_ID}
-      export GOOGLE_CLOUD_PROJECT=${PROJECT_ID}
-    elif [ -n "${GOOGLE_CLOUD_PROJECT}" ]
-    then
-      gcloud_ext config set project ${GOOGLE_CLOUD_PROJECT}
-    else
-      GOOGLE_CLOUD_PROJECT=$(gcloud config get project)
-      if [ -z "${GOOGLE_CLOUD_PROJECT}" ]
-      then
-        echo "Aborting execution: prerequisites have not been met."
-        echo "- Google Cloud Project not defined."
-        echo "  Please run 'gcloud config set project <PROJECT_ID>' and try again."
-        exit 1
-      fi
-    fi
+    validate_values
 
-    if [ -z "${CONTAINER_CONFIG}" ]
+    if [ "${MODE}" == "remove" ]
     then
-      echo "Aborting execution: prerequisites have not been met."
-      echo "- CONTAINER_CONFIG is required."
-      echo "  In Tag Manager, navigate to your server container workspace and click on"
-      echo "  the container ID at the top-right of the page. Click on "
-      echo "  'Manually provision tagging server' to find the Container Config value."
-      exit 1
-    fi
-
-    IFS=',' read -ra REGIONS_LIST <<< ${REGIONS}
-
-    if [ -z "${DELETE}" ]
-    then
-      if [ ${#REGIONS_LIST[*]} -gt 1 ]
-      then
-        if [ -z "${DOMAIN_NAME}" ]
-        then
-          echo "Aborting execution: prerequisites have not been met."
-          echo "- DOMAIN_NAME is required if using multi-region support. Please fix and try again."
-          exit 1
-        fi
-      fi
-      echo "Installing sGTM in ${GOOGLE_CLOUD_PROJECT}:"
-
-      enable_apis
-      deploy_cloud_run
-      if [ -n "${DOMAIN_NAME}" ]
-      then
-        create_load_balancer
-        create_network_endpoint_groups
-      fi
-    else
-      echo "Deleting sGTM from ${GOOGLE_CLOUD_PROJECT}:"
+      echo "Deleting the tagging server from ${GOOGLE_CLOUD_PROJECT}:"
+      delete_log_filters
       if [ -n "${DOMAIN_NAME}" ]
       then
         delete_network_endpoint_groups
         delete_load_balancer
       fi
       delete_cloud_run
+    elif [ "${MODE}" == "show_address" ]
+    then
+      describe_address
+    elif [ "${MODE}" == "deploy_testing" ]
+    then
+      echo "Installing the tagging server in testing mode in ${GOOGLE_CLOUD_PROJECT}:"
+      enable_apis
+      deploy_cloud_run
+      create_log_filters
+    elif [ "${MODE}" == "remove_testing" ]
+    then
+      echo "Deleting the tagging server in testing mode from ${GOOGLE_CLOUD_PROJECT}:"
+      delete_log_filters
+      delete_cloud_run
+    elif [ "${MODE}" == "script_serving" ]
+    then
+      echo "Creating a specific backend service for first party script serving in ${GOOGLE_CLOUD_PROJECT}:"
+      create_script_serving_backend_service
+    elif [ "${MODE}" == "remove_script_serving" ]
+    then
+      echo "Deleting the backend service used for first party script serving from ${GOOGLE_CLOUD_PROJECT}:"
+      delete_script_serving_backend_service
+    elif [ "${MODE}" == "deploy" ]
+    then
+      echo "Installing the tagging server in ${GOOGLE_CLOUD_PROJECT}:"
+      enable_apis
+      deploy_cloud_run
+      create_load_balancer
+      create_network_endpoint_groups
+      create_log_filters
+    else
+      print_usage
     fi
 }
 
--- tutorial.md
+++ tutorial.md
@@ -1,16 +1,21 @@
-# Server Side GTM Autodeployer
+# Tagging server auto-deployment guide
 
 ## Setup
 
-Welcome to the guided deployment of server side GTM (sGTM). This would help you
-deploy sGTM as a Cloud Run service, customized for your particular environment.
+This deployment guide helps you:
 
-## Cloud Project
+-   Deploy a new Cloud Run server
+-   Install the server-side Tag Manager container
+-   Customize the server to meet your needs.
 
-To start we need to select the Google Cloud Project to deploy sGTM in.
+## Cloud project
 
-We'll be using a script to deploy sGTM on Google Cloud, this uses the Google Cloud
-SDK that should be available directly from your Cloud Shell environment.
+To prepare, you need to select the Google Cloud Project to deploy the tagging
+server in.
+
+You'll be using a script to deploy the tagging server on Cloud Run. It uses the
+Google Cloud SDK that should be available directly from your Cloud Shell
+environment.
 
 <walkthrough-project-setup></walkthrough-project-setup>
 
@@ -24,50 +29,210 @@ export GOOGLE_CLOUD_PROJECT=<walkthrough-project-id/>
 
 ## Customize the environment
 
-To deploy sGTM using the best settings for your environment, we need
-to gather some information from your environment.
+To deploy the tagging server using the best settings for your environment, you
+need to provide some information about your environment.
 
 Open <walkthrough-editor-open-file filePath="./config.conf">
 config.conf</walkthrough-editor-open-file>
 
-Each variable modifies a specific behaviour in the deployment script.
-Make sure you provide the appropiate values and save the file before
-continuing.
+Each variable modifies a specific behavior in the deployment script.
 
-You can find a summary of each value below.
+Click next to start the review process for each of them.
 
-Variable                         | Description
--------------------------------: | :-------------------------------------------------------- |
-CONTAINER_CONFIG                 | Add the configuration string for the server container.
-REGIONS                          | Specify the regions to deploy sGTM, separated by commas.
-DOMAIN_NAME                      | Define the custom domain you want to use. This is required when specifying multiple regions, otherwise it is optional.
-USE_1P_SCRIPT_SERVING            | `"YES"` if you want to use First Party Script Serving.
-ENABLE_LOGGING                   | `"YES"` if you want to enable logging.
+## Container config string
 
-After that, let's get the deployment started.
+The first item you need to provide is the the configuration string for the
+server container. In Tag Manager, navigate to your server container workspace
+and click on the container ID at the top-right of the page. Click on **Manually
+provision** tagging server to find the **container config** value.
+
+Then, copy this value into the
+<walkthrough-editor-select-regex filePath="./config.conf" regex='CONTAINER_CONFIG=".*"'>
+`CONTAINER_CONFIG`</walkthrough-editor-select-regex> variable.
+
+Example:
+
+```js
+CONTAINER_CONFIG="WW91IGFyZSB0b28gc21hcnQgZm9yIHRoZSB0dXRvcmlhbCwgZ28gZm9yIHRoZSBzY3JpcHQhIDsp"
+```
+
+## Deployment regions
+
+The tagging server can be deployed on any region supporting Cloud Run. For this
+deployment, you can specify one or more regions where the tagging server will be
+deployed. You can find the list
+[here](https://cloud.google.com/run/docs/locations).
+
+To define the regions to be used, please add them separated by commas (without
+spaces) in the
+<walkthrough-editor-select-regex filePath="./config.conf" regex='REGIONS=".*"'>
+`REGIONS`</walkthrough-editor-select-regex> variable.
+
+Example:
+
+```js
+REGIONS="europe-west1,europe-west2"
+```
+
+## Domain name
+
+Next one is the domain name you want to use with the tagging server. Please
+specify the domain name in the
+<walkthrough-editor-select-regex filePath="./config.conf" regex='DOMAIN_NAME=".*"'>
+`DOMAIN_NAME`</walkthrough-editor-select-regex> variable.
+
+Example:
+
+```js
+DOMAIN_NAME="metrics.example.com"
+```
+
+## Maximum number of instances
+
+You can specify the maximum number of instances each Cloud Run deployment can
+scale to in the
+<walkthrough-editor-select-regex filePath="./config.conf" regex='MAX_INSTANCES=.*'>
+`MAX_INSTANCES`</walkthrough-editor-select-regex> variable. It only establishes
+a maximum boundary, and it would scale up to that number if there are enough
+requests requiring it.
+
+Example:
+
+```js
+MAX_INSTANCES=10
+```
+
+We expect that autoscaling 2-10 servers will handle 35-350 requests per second,
+though the performance will vary with the number of tags, and what those tags
+do. If you expect to handle more than 350 per second at a given time, we
+recommend increasing the number above 10.
+
+## Logging level
+
+*This step is optional.*
+
+The last step before deployment is selecting the level of logging you want for
+the tagging server. By default, logging levels are not modified, but if your
+tagging server handles a lot of requests per month (e.g. greater than 1
+million), those log messages may incur significant logging charges.
+
+There are two options available to alleviate this issue:
+
+-   `DISABLE`: Disable the logging of requests.
+-   `ERROR_ONLY` Log only ERROR level messages.
+
+Make sure to set the appropriated value in the
+<walkthrough-editor-select-regex filePath="./config.conf" regex='LOGGING=".*"'>
+`LOGGING`</walkthrough-editor-select-regex> variable.
+
+Example:
+
+```js
+LOGGING="DISABLE"
+```
 
 ## Deploy it
 
 The script `deploy.sh` will read the configuration you prepared in the previous
-step and take care of everything needed for sGTM. You can execute the commands
-by clicking the Cloud Shell icon next to them to copy the command to your shell,
-and then run it from the shell by pressing Enter/Return.
+step and take care of everything needed for deploying the tagging server. You
+can execute the commands by clicking the Cloud Shell icon next to them to copy
+the command to your shell, and then run it from the shell by pressing
+Enter/Return.
 
-### Dry Run (Optional)
+### Dry run (optional)
 
-If you want to preview all the commands that will be executed before
-doing any modifications to the system, you can run the script
-in *test* mode (without doing any modifications) using this command:
+If you want to preview all the commands that will be executed before doing any
+modifications to the system, you can run the script in *test* mode (without
+doing any modifications) using this command:
 
 ```bash
-bash deploy.sh -t
+bash deploy.sh -d
 ```
 
 ### Deployment
 
-You can execute the script by using this command  (make sure to follow the
-output in case any errors occur):
+You can execute the script by using this command (make sure to follow the output
+in case any errors occur):
 
 ```bash
 bash deploy.sh
 ```
+
+## Update DNS records at your domain registrar
+
+Now that the server has been deployed, you need to update the DNS records of the
+domain to point to your load balancer IP.
+
+To get the IP you need to use, you can run the following command:
+
+```bash
+bash deploy.sh -m show_address
+```
+
+Next, log in to your account at your domain registrar and open the DNS
+configuration page.
+
+Locate the host records section of your domain’s configuration page and add
+create or update a **A** record with the following information:
+
+-   Record name: Enter only the subdomain part from the custom domain
+    you defined in previous steps. For example, enter “metrics” to map
+    metrics.example.com.
+
+-   Data: Enter the IP address you got from the previous command.
+
+Save your changes in the DNS configuration page of your domain’s account. In
+most cases, it takes only a few minutes for these changes to take effect, but in
+some cases it can take several hours.
+
+## First-party script serving
+
+*This step is optional.*
+
+Do you want to enable first-party script serving?
+
+To establish a first-party context between your web container and your tagging
+server, Google scripts must be loaded through your server.
+
+To enable first-party script serving, follow the steps described
+[here](https://developers.google.com/tag-platform/tag-manager/server-side/dependency-serving?tag=gtm#before_you_begin).
+
+Note: By default, Tag Manager or the Google tag (gtag.js) load their
+dependencies from Google-owned servers, such as www.googletagmanager.com. You
+need to update the script URL on your website to load dependencies through your
+own server.
+
+### Region-specific settings
+
+When the previous steps are ready, the only part missing is setting up
+region-specific tag behavior for even more control (this is specially important
+if using consent mode).
+
+This configuration requires using custom headers when serving the scripts. As
+this implies a cost per 1M requests, you'll deploy a supplemental backend
+service with the headers configuration, while keeping the original Tag Manager
+backend service without them (you can find more details
+[here](https://developers.google.com/tag-platform/tag-manager/server-side/enable-region-specific-settings#step_1_set_up_the_request_header_)).
+
+To deploy the supplemental backend to handle script requests, you need to
+execute the following command:
+
+```sh
+bash deploy.sh -m script_serving
+```
+
+After everything is ready, please follow the rest of the steps described
+[here](https://developers.google.com/tag-platform/tag-manager/server-side/enable-region-specific-settings).
+
+## Deployment finished
+
+<walkthrough-conclusion-trophy></walkthrough-conclusion-trophy>
+
+Congratulations! You have successfully deployed a tagging server in your own GCP
+project.
+
+### Next steps
+
+-   [How to send data to server-side Tag Manager](https://developers.google.com/tag-platform/tag-manager/server-side/send-data)
+-   [Server-side tagging fundamentals](https://developers.google.com/tag-platform/learn/sst-fundamentals)
+-   [Troubleshooting](https://developers.google.com/tag-platform/tag-manager/server-side/debug)
